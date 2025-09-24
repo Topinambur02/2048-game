19b362491eb4cdb44f62409541a8b4dbe4d63ec5
Алекс Тырданов <alekstyrdanov@Alekss-Mac-mini.local>
2025-09-24 22:16:53 +0300

    #3 feat: add main page

--- frontend/app/index.tsx
+++ frontend/app/index.tsx
@@ -1,9 +1,40 @@
-import { View, Text } from "react-native"
+import { IndexScreenNavigationProp } from "@/assets/props/IndexScreenNavigationProp"
+import { useNavigation } from "@react-navigation/native"
+import React from "react"
+import { Text, TouchableOpacity, View } from "react-native"
+import { styles } from "@/assets/styles/MainPageStyles"
+import NumberBlock from "@/assets/components/NumberBlock"
 
-export default function HomeScreen() {
+export default function Index() {
+  const navigation = useNavigation<IndexScreenNavigationProp>()
+
   return (
-    <View>
-      <Text>Home Screen</Text>
+    <View style={styles.container}>
+
+      <View style={styles.gridContainer}>
+        <View style={styles.row}>
+          <NumberBlock number="2" color="#EDE0C8" isDarkText={true} />
+          <NumberBlock number="0" color="#EDC850" isDarkText={false} />
+        </View>
+        <View style={styles.row}>
+          <NumberBlock number="4" color="#F67C5F" isDarkText={false} />
+          <NumberBlock number="8" color="#F65E3C" isDarkText={false} />
+        </View>
+      </View>
+
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => navigation.navigate('Game')}
+      >
+        <Text style={styles.buttonText}>Играть</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => navigation.navigate('Leaderboard')}
+      >
+        <Text style={styles.buttonText}>Таблица лидеров</Text>
+      </TouchableOpacity>
     </View>
   )
 }
---
+++ frontend/assets/props/NumberBlockProps.ts
@@ -0,0 +1,5 @@
+export interface NumberBlockProps {
+    number: string
+    color: string
+    isDarkText: boolean
+}
